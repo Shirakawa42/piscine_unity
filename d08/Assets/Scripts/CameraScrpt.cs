c068810e00c95b5fbb839f2f1723c3b20eae4764
Lucas VASSEUR <lvasseur@e3r13p11.42.fr>
2018-10-11 17:18:33 +0200

    jaime pas les anims

---
+++ d08/Assets/Scripts/CameraScrpt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScrpt : MonoBehaviour {
+	
+	public GameObject	player;
+
+	void Update () {
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 15, player.transform.position.z - 10);
+	}
+}
---
+++ d08/Assets/Scripts/MayaScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MayaScript : MonoBehaviour {
+
+	private Animator		anim;
+	private NavMeshAgent	NMA;
+	private Ray				ray;
+	private RaycastHit		hit;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		NMA = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit)) {
+				if (hit.collider.tag == "Terrain") {
+					NMA.SetDestination(hit.point);
+				}
+				anim.SetTrigger("run");
+			}
+		}
+		if (Vector3.Distance(NMA.destination, transform.position) <= 0.5f) {
+			anim.ResetTrigger("run");
+		}
+	}
+}
